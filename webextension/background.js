5f00ca71ab6dd049807e7e6c19fa8630e93c1af8
Matej Dian <Snoop05B@gmail.com>
2018-11-21 00:19:07 +0100

    Add link support

--- webextension/background.js
+++ webextension/background.js
@@ -1,7 +1,7 @@
 browser.contextMenus.create({
 	id: "playwithmpv",
 	title: "Play with mpv",
-	contexts: ["tab"],
+	contexts: ["link", "tab"],
 	icons: {
 		"32": "icons/mpv-32.png"
 	}
@@ -10,7 +10,11 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener((info, tab) => {
 	switch (info.menuItemId) {
 	case "playwithmpv":
-		browser.runtime.sendNativeMessage("playwithmpv", tab.url);
+		if (info.linkUrl) {
+			browser.runtime.sendNativeMessage("playwithmpv", info.linkUrl);
+		} else {
+			browser.runtime.sendNativeMessage("playwithmpv", tab.url);
+		}
 		break;
 	}
 });
